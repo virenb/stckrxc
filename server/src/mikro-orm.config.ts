92f1e5579e184a3678aeaec923c8dc88607e2b83
virenb <virenb@gmail.com>
2020-09-17 16:44:34 -0400

    Added user resolver and entity

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,17 +1,16 @@
-import { MikroORM } from '@mikro-orm/core';
-import path from 'path';
-import { __prod__ } from './constants';
-import { Post } from './entities/Post';
+import path from "path";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { User } from "./entities/User";
 
 export default {
   migrations: {
-    path: path.join(__dirname, './migrations'),
+    path: path.join(__dirname, "./migrations"),
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
-  dbName: 'stckrxc',
-  type: 'postgresql',
-  debug: !__prod__
+  entities: [Post, User],
+  dbName: "stckrxc",
+  type: "postgresql",
+  debug: !__prod__,
 } as Parameters<typeof MikroORM.init>[0];
-
-
