745d12553ec57f2691b9736108e3bc6c388b520d
virenb <virenb@gmail.com>
2020-09-16 21:21:52 -0400

    Set up MikroORM and postgres db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello TS world');
+import { MikroORM } from "@mikro-orm/core";
+import { Post } from './entities/Post';
+import mikroConfig from './mikro-orm.config'
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
+
+  // const post = orm.em.create(Post, { title: 'First Post' });
+  // await orm.em.persistAndFlush(post);
+
+  const posts = await orm.em.find(Post, {});
+  console.log(posts)
+
+};
+
+
+main().catch(err => {
+  console.error(err);
+});
