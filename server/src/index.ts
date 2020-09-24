3956b891f5042939b5d36fb3f4b7a7104c04250f
virenb <virenb@gmail.com>
2020-09-24 15:27:20 -0400

    Updated server, generate token for change pw

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
 import 'reflect-metadata';
 import { UserResolver } from './resolvers/user';
-import redis from 'redis'
+import Redis from 'ioredis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 import { COOKIE_NAME, __prod__ } from './constants';
@@ -18,8 +18,8 @@ const main = async () => {
   await orm.getMigrator().up();
 
   const app = express();
-  const RedisStore = connectRedis(session)
-  const redisClient = redis.createClient();
+  const RedisStore = connectRedis(session);
+  const redis = new Redis();
 
   // Order matters for express middleware! Redis comes before Apollo
 
@@ -34,7 +34,7 @@ const main = async () => {
     session({
       name: COOKIE_NAME,
       store: new RedisStore({
-        client: redisClient,
+        client: redis,
         disableTouch: true,
       }),
       cookie: {
@@ -54,7 +54,7 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false
     }),
-    context: ({ req, res }) => ({ em: orm.em, req, res }),
+    context: ({ req, res }) => ({ em: orm.em, req, res, redis }),
   })
 
   apolloServer.applyMiddleware({
